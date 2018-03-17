0a2035d98261dbec933846d4a49ff5430f4b10bd
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-03-17 19:50:51 +0700

    Add dependencies packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+
+import 'semantic-ui-css/semantic.min.css'
+
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+		  <App />
+		</AppContainer>,
+		document.getElementById('root')
+	)
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
